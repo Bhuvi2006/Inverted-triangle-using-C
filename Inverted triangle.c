98b0be7e166be050a6c9890c3bca6450850fd35a
Bhuvi21 <bhuvijai2006@gmail.com>
2024-07-23 08:26:56 +0530

    Add files via upload

---
+++ Inverted triangle.c
@@ -0,0 +1,18 @@
+//Inverted triangle
+int main()
+{
+    int i,j,n;
+    printf("Enter the number of rows: ");
+    scanf("%d",&n);
+for (i=n;i>=1;i--)
+{
+    for(j=1;j<=i;j++)
+    {
+        printf(" * ");
+        
+    }
+    printf("\n");
+}
+    
+    
+}
